155d534028e681fe810a967775922a6a2f4119eb
tanveerisonline <tanveerisonline@gmail.com>
2023-03-31 17:12:42 +0530

    Add crud functionality to blog app

--- db/migrate/20230328180336_add_foreign_key_to_likes.rb
+++ db/migrate/20230328180336_add_foreign_key_to_likes.rb
@@ -3,6 +3,6 @@ class AddForeignKeyToLikes < ActiveRecord::Migration[7.0]
   add_foreign_key :likes, :posts, column: :post_id
   add_foreign_key :likes, :users, column: :author_id
   add_index :likes, :post_id
-  add_index :likes, :user_id
+  add_index :likes, :author_id
   end
 end
