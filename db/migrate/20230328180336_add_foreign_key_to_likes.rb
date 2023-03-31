d41f74af25973695098201b347d0ff803e35305c
tanveerisonline <tanveerisonline@gmail.com>
2023-03-31 16:41:45 +0530

    Add requested changes by code reviewer

--- db/migrate/20230328180336_add_foreign_key_to_likes.rb
+++ db/migrate/20230328180336_add_foreign_key_to_likes.rb
@@ -1,7 +1,7 @@
 class AddForeignKeyToLikes < ActiveRecord::Migration[7.0]
   def change
   add_foreign_key :likes, :posts, column: :post_id
-  add_foreign_key :likes, :users, column: :user_id
+  add_foreign_key :likes, :users, column: :author_id
   add_index :likes, :post_id
   add_index :likes, :user_id
   end
